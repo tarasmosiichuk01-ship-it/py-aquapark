b4c4d39af51f45d2cd09a05815d5bf07896e84b4
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2025-12-26 19:15:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
+from __future__ import annotations
 from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: str, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: IntegerRange, owner: str) -> int:
+        return getattr(instance, self.protected_name, None)
+
+    def __set__(self, instance: IntegerRange, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError
+        if value not in range(self.min_amount, self.max_amount + 1):
+            raise ValueError
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
@@ -14,12 +34,33 @@ class SlideLimitationValidator(ABC):
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(
+            self,
+            name: str,
+            limitation_class: type[SlideLimitationValidator]
+    ) -> None:
+
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        try:
+            limitation_instance = self.limitation_class()
+            limitation_instance.age = visitor.age
+            limitation_instance.height = visitor.height
+            limitation_instance.weight = visitor.weight
+            return True
+        except (TypeError, ValueError):
+            return False
